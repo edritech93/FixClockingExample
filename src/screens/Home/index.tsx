ea0e6a0f553a9962e62dfb071bec9f09d966ef3e
Yudi Alviska <yudi.a@weefer.co.id>
2024-02-20 10:14:09 +0700

    clean code on Home Screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -8,10 +8,7 @@ import {
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {detectFromBase64} from 'vision-camera-face-detection';
 import {launchImageLibrary} from 'react-native-image-picker';
-import {useTensorflowModel} from 'react-native-fast-tflite';
 import {RootStackType} from '../../types/RootStackType';
-import {base64ToFloat32Array} from '../../libs/processing';
-import {decode} from 'base64-arraybuffer-es6';
 import {Button} from 'react-native-paper';
 
 interface IHome extends NativeStackScreenProps<RootStackType, 'Home'> {}
@@ -19,16 +16,10 @@ interface IHome extends NativeStackScreenProps<RootStackType, 'Home'> {}
 export default function Home(props: IHome) {
   const {navigation} = props;
 
-  const [faceBase64, setFaceBase64] = useState('');
-  const [faceBase64R, setFaceBase64R] = useState('');
-  const [tensorSample, setTensorSample] = useState<number[]>([]);
-  const [tensorR, setTensorR] = useState<number[]>([]);
-
-  const fileModel = useTensorflowModel(
-    require('../../assets/mobile_face_net.tflite'),
-    'core-ml',
-  );
-  const model = fileModel.state === 'loaded' ? fileModel.model : undefined;
+  const [faceSample1, setFaceSample1] = useState('');
+  const [faceSample2, setFaceSample2] = useState('');
+  const [tensorData1, setTensorData1] = useState<number[]>([]);
+  const [tensorData2, setTensorData2] = useState<number[]>([]);
 
   useEffect(() => {
     async function _getPermission() {
@@ -48,7 +39,7 @@ export default function Home(props: IHome) {
     _getPermission();
   }, []);
 
-  const _onOpenImage = async () => {
+  const _onOpenSample1 = async () => {
     await getPermissionReadStorage().catch((error: Error) => {
       console.log(error);
       return;
@@ -65,41 +56,21 @@ export default function Home(props: IHome) {
       result.assets &&
       result.assets.length > 0 &&
       result.assets[0]?.uri &&
-      result.assets[0]?.base64 &&
-      model
+      result.assets[0]?.base64
     ) {
-      const base64Face = await detectFromBase64(result.assets[0].base64).catch(
+      const base64Sample1 = result.assets[0].base64;
+      setFaceSample1(base64Sample1);
+      const objFace1 = await detectFromBase64(base64Sample1).catch(
         (error: Error) => {
           console.log(error);
           return;
         },
       );
-      if (!base64Face) {
-        console.log('no face');
-        return;
-      }
-      const bufferLength = 112;
-      const arrayBuffer: ArrayBuffer = decode(base64Face, {
-        maxByteLength: bufferLength,
-      });
-      console.log('arrayBuffer.byteLength => ', arrayBuffer.byteLength);
-      const array = new Float32Array(arrayBuffer, 0, bufferLength);
-      console.log('array => ', array.buffer.byteLength);
-      const output = model.runSync([array] as any);
-      console.log('output => ', output);
-
-      // setFaceBase64(base64Face);
-      // const array: Float32Array = base64ToFloat32Array(base64Face);
-      // const output = model.runSync([array] as any);
-      // console.log('output => ', output);
-      // const arrayTensor: number[] = [];
-      // output[0].map((e: any) => arrayTensor.push(e));
-      // setTensorSample(arrayTensor);
-      // console.log('arrayTensor => ', arrayTensor);
+      console.log('objFace1 => ', objFace1);
     }
   };
 
-  const _onOpenImageR = async () => {
+  const _onOpenSample2 = async () => {
     await getPermissionReadStorage().catch((error: Error) => {
       console.log(error);
       return;
@@ -116,37 +87,25 @@ export default function Home(props: IHome) {
       result.assets &&
       result.assets.length > 0 &&
       result.assets[0]?.uri &&
-      result.assets[0]?.base64 &&
-      model
+      result.assets[0]?.base64
     ) {
-      const base64Face = await detectFromBase64(result.assets[0].base64).catch(
+      const base64Sample2 = result.assets[0].base64;
+      setFaceSample2(base64Sample2);
+      const objFace2 = await detectFromBase64(base64Sample2).catch(
         (error: Error) => {
           console.log(error);
           return;
         },
       );
-      if (!base64Face) {
-        console.log('no face');
-        return;
-      }
-      setFaceBase64R(base64Face);
-      const array: Float32Array = base64ToFloat32Array(base64Face);
-      const output = model.runSync([array] as any);
-      console.log('output => ', output);
-      // const arrayTensor: number[] = [];
-      // output[0].map((e: any) => arrayTensor.push(e));
-      // setTensorR(arrayTensor);
-      // console.log('arrayTensor => ', arrayTensor);
+      console.log('objFace2 => ', objFace2);
     }
   };
 
   function _onTensor() {
-    console.log('tensorSample => ', tensorSample.length);
-    console.log('tensorR => ', tensorR.length);
-    for (let index = 0; index < tensorSample.length; index++) {
+    for (let index = 0; index < tensorData1.length; index++) {
       let distance = 0.0;
-      for (let i = 0; i < tensorR.length; i++) {
-        const diff = tensorR[i] - tensorSample[i];
+      for (let i = 0; i < tensorData2.length; i++) {
+        const diff = tensorData2[i] - tensorData1[i];
         distance += diff * diff;
       }
       console.log(`${new Date().toTimeString()} = `, distance);
@@ -155,34 +114,34 @@ export default function Home(props: IHome) {
 
   return (
     <View style={styles.container}>
-      <Button mode={'contained'} onPress={_onOpenImage}>
-        Add Image Sample
-      </Button>
-      {faceBase64.length > 0 && (
-        <Image
-          source={{uri: `data:image/png;base64,${faceBase64}`}}
-          style={styles.imgFace}
-        />
-      )}
-      <Button mode={'contained'} onPress={_onOpenImageR}>
-        Add Image R
-      </Button>
-      {faceBase64R.length > 0 && (
-        <Image
-          source={{uri: `data:image/png;base64,${faceBase64R}`}}
-          style={styles.imgFace}
-        />
-      )}
+      <View style={styles.wrapSample}>
+        <Button mode={'contained'} onPress={_onOpenSample1}>
+          Add Image Sample 1
+        </Button>
+        {faceSample1.length > 0 && (
+          <Image
+            source={{uri: `data:image/png;base64,${faceSample1}`}}
+            style={styles.imgFace}
+          />
+        )}
+        <Button mode={'contained'} onPress={_onOpenSample2}>
+          Add Image Sample 2
+        </Button>
+        {faceSample2.length > 0 && (
+          <Image
+            source={{uri: `data:image/png;base64,${faceSample2}`}}
+            style={styles.imgFace}
+          />
+        )}
+        <Button mode={'contained'} onPress={() => _onTensor()}>
+          Do Tensor
+        </Button>
+      </View>
       <Button
         mode={'contained'}
-        onPress={() => _onTensor()}
-        // onPress={() => navigation.push('Clocking', {tensorSample})}
-      >
-        Test Clocking
-      </Button>
-      <Button
-        mode={'contained'}
-        onPress={() => navigation.push('Clocking', {tensorSample})}>
+        onPress={() =>
+          navigation.push('Clocking', {tensorSample: tensorData1})
+        }>
         Goto Clocking
       </Button>
     </View>
@@ -192,8 +151,10 @@ export default function Home(props: IHome) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
+  },
+  wrapSample: {
+    flex: 1,
     gap: 16,
   },
   imgFace: {
