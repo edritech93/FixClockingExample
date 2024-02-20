201a0a1d94338bf4c11c12a99dea1508ec60da69
Yudi Alviska <yudi.a@weefer.co.id>
2024-02-20 15:20:24 +0700

    remove package resize and tflite

--- src/screens/Clocking/index.tsx
+++ src/screens/Clocking/index.tsx
@@ -22,8 +22,8 @@ import Animated, {
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {Worklets, useSharedValue} from 'react-native-worklets-core';
 import {ActivityIndicator, Button, Text} from 'react-native-paper';
-import {useResizePlugin} from 'vision-camera-resize-plugin';
-import {useTensorflowModel} from 'react-native-fast-tflite';
+// import {useResizePlugin} from 'vision-camera-resize-plugin';
+// import {useTensorflowModel} from 'react-native-fast-tflite';
 import {RootStackType} from '../../types/RootStackType';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
@@ -58,12 +58,12 @@ export default function Clocking(props: IClocking) {
     {photoResolution: 'max'},
   ]);
   const fps = Math.min(format?.maxFps ?? 1, targetFps);
-  const fileModel = useTensorflowModel(
-    require('../../assets/mobile_face_net.tflite'),
-    'core-ml',
-  );
-  const model = fileModel.state === 'loaded' ? fileModel.model : undefined;
-  const {resize} = useResizePlugin();
+  // const fileModel = useTensorflowModel(
+  //   require('../../assets/mobile_face_net.tflite'),
+  //   'core-ml',
+  // );
+  // const model = fileModel.state === 'loaded' ? fileModel.model : undefined;
+  // const {resize} = useResizePlugin();
   const rectWidth = useSharedValue(100); // rect width
   const rectHeight = useSharedValue(100); // rect height
   const rectX = useSharedValue(100); // rect x position
@@ -90,62 +90,46 @@ export default function Clocking(props: IClocking) {
     _getPermission();
   }, []);
 
-  const frameProcessor = useFrameProcessor(
-    (frame: Frame) => {
-      'worklet';
-      // const start = performance.now();
-      const dataFace: FaceType = scanFaces(frame);
-      // NOTE: handle face detection
-      if (model && dataFace && dataFace.bounds) {
-        const {width: frameWidth, height: frameHeight} = frame;
-        const xFactor = SCREEN_WIDTH / frameWidth;
-        const yFactor = SCREEN_HEIGHT / frameHeight;
-        const bounds: FaceBoundType = dataFace.bounds;
-        rectWidth.value = bounds.width * xFactor;
-        rectHeight.value = bounds.height * yFactor;
-        rectX.value = bounds.x * xFactor;
-        rectY.value = bounds.y * yFactor;
-        updateRect({
-          width: rectWidth.value,
-          height: rectHeight.value,
-          x: rectX.value,
-          y: rectY.value,
-        });
-        // NOTE: handle resize frame
-        const arrayBuffer = resize(frame, {
-          crop: {
-            x: rectX.value,
-            y: rectY.value,
-            width: 112,
-            height: 112,
-          },
-          scale: {
-            width: 112,
-            height: 112,
-          },
-          pixelFormat: 'rgb',
-          dataType: 'float32',
-        });
-        const array: Float32Array = new Float32Array(arrayBuffer);
-        // console.log('array => ', array);
-        const output = model.runSync([array] as any[]);
-        console.log('output => ', output);
-        // const arrayTensor: number[] = [];
-        // output[0].map((e: any) => arrayTensor.push(e));
-        // for (let index = 0; index < arraySample.length; index++) {
-        //   let distance = 0.0;
-        //   for (let i = 0; i < arrayTensor.length; i++) {
-        //     const diff = arrayTensor[i] - arraySample[i];
-        //     distance += diff * diff;
-        //   }
-        //   updateDistance(distance);
-        // }
-        // const end = performance.now();
-        // console.log(`Performance: ${end - start}ms`);
-      }
-    },
-    [model],
-  );
+  const frameProcessor = useFrameProcessor((frame: Frame) => {
+    'worklet';
+    // const start = performance.now();
+    const dataFace: FaceType = scanFaces(frame);
+    // NOTE: handle face detection
+    if (dataFace && dataFace.bounds) {
+      const {width: frameWidth, height: frameHeight} = frame;
+      const xFactor = SCREEN_WIDTH / frameWidth;
+      const yFactor = SCREEN_HEIGHT / frameHeight;
+      const bounds: FaceBoundType = dataFace.bounds;
+      rectWidth.value = bounds.width * xFactor;
+      rectHeight.value = bounds.height * yFactor;
+      rectX.value = bounds.x * xFactor;
+      rectY.value = bounds.y * yFactor;
+      updateRect({
+        width: rectWidth.value,
+        height: rectHeight.value,
+        x: rectX.value,
+        y: rectY.value,
+      });
+      // NOTE: handle resize frame
+      // const arrayBuffer = resize(frame, {
+      //   crop: {
+      //     x: rectX.value,
+      //     y: rectY.value,
+      //     width: 112,
+      //     height: 112,
+      //   },
+      //   scale: {
+      //     width: 112,
+      //     height: 112,
+      //   },
+      //   pixelFormat: 'rgb',
+      //   dataType: 'float32',
+      // });
+      // const array: Float32Array = new Float32Array(arrayBuffer);
+      // const output = model.runSync([array] as any[]);
+      // console.log('output => ', output);
+    }
+  }, []);
 
   const faceAnimStyle = useAnimatedStyle(() => {
     return {
